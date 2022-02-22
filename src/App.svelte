81561d3099987c7ac233b88a9696499c2cd60e6b
Benjamin Keating <ben@spreadsheetsusa.com>
2022-02-22 06:05:13 -0600

    format, favicon, copy

--- src/App.svelte
+++ src/App.svelte
@@ -8,7 +8,7 @@
   import IoIosMoon from 'svelte-icons/io/IoIosMoon.svelte';
   import IoIosSunny from 'svelte-icons/io/IoIosSunny.svelte';
   import IoIosCar from 'svelte-icons/io/IoIosCar.svelte';
-	import FaMapMarkedAlt from 'svelte-icons/fa/FaMapMarkedAlt.svelte';
+  import FaMapMarkedAlt from 'svelte-icons/fa/FaMapMarkedAlt.svelte';
 
   import Logo from './Logo.svelte';
 
@@ -16,6 +16,7 @@
   let mapComponent;
   let menuIsOpen = false;
   let isDarkMode = false;
+  let showSplash = true;
 
   $: mapStyle = 'mapbox://styles/mapbox/outdoors-v11';
   $: lng = -88.4051;
@@ -50,63 +51,96 @@
   onMount(() => {
     mapComponent?.flyTo({ center: [lng, lat], zoom: 14, bearing: 0, pitch: 45, speed: 0.8, curve: 0.5 });
     let map = mapComponent?.getMap();
-		const ne = [42.84103774367695, -88.49734216528779];
-		const sw = [42.70040771258098, -88.2800188683067];
-		map?.getMap().setMaxBounds([sw, ne]);
+    const ne = [42.84103774367695, -88.49734216528779];
+    const sw = [42.70040771258098, -88.2800188683067];
+    map?.getMap().setMaxBounds([sw, ne]);
   });
 
   const handleOpenMenu = () => {
     menuIsOpen = !menuIsOpen;
+    showSplash = false;
   };
   const handleNightLife = () => {
     mapComponent?.flyTo({ center: easttroyCenter, zoom: 15, pitch: 0, bearing: 0, speed: 1, curve: 1 }); // starting point
     handleAppearanceMode(true); // make it dark
   };
 
-	function rotateCamera(timestamp) {
-		let m = mapComponent?.getMap();
-		// clamp the rotation between 0 -360 degrees
-		// Divide timestamp by 100 to slow rotation to ~10 degrees / sec
-		m.rotateTo((timestamp / 100) % 360, { duration: 0 });
-		// Request the next frame of the animation.
-		requestAnimationFrame(rotateCamera);
-	}
+  function rotateCamera(timestamp) {
+    let m = mapComponent?.getMap();
+    // clamp the rotation between 0 -360 degrees
+    // Divide timestamp by 100 to slow rotation to ~10 degrees / sec
+    m.rotateTo((timestamp / 100) % 360, { duration: 0 });
+    // Request the next frame of the animation.
+    requestAnimationFrame(rotateCamera);
+  }
 
   const handleCruiseNight = () => {
-		let currentStep = 0;
-		let tourPoints = [
-			{ label: 'Gus\'s Drive-in', center: [42.783958493217334, -88.41788905454426].reverse(), zoom: 18, bearing: 80, pitch: 60, rotate: false, duration: 0 },
-			{ label: 'East Troy Airport', center: [42.79722849966591, -88.37596611461682].reverse(), zoom: 16, bearing: 0, pitch: 0, rotate: true, duration: 60000 },
-			{ label: 'Village Square Park', center: [42.78477231354394, -88.40545403606905].reverse(), zoom: 16, bearing: 0, pitch: 0, rotate: true, duration: 10000 },
-		]
+    let currentStep = 0;
+    let tourPoints = [
+      {
+        label: "Gus's Drive-in",
+        center: [42.783958493217334, -88.41788905454426].reverse(),
+        zoom: 18,
+        bearing: 80,
+        pitch: 60,
+        rotate: false,
+        duration: 0,
+      },
+      {
+        label: 'East Troy Airport',
+        center: [42.79722849966591, -88.37596611461682].reverse(),
+        zoom: 16,
+        bearing: 0,
+        pitch: 0,
+        rotate: true,
+        duration: 60000,
+      },
+      {
+        label: 'Village Square Park',
+        center: [42.78477231354394, -88.40545403606905].reverse(),
+        zoom: 16,
+        bearing: 0,
+        pitch: 0,
+        rotate: true,
+        duration: 10000,
+      },
+    ];
     // mapComponent?.flyTo({ center: cruiseNightStart, zoom: 18, pitch: 60, bearing: 80, speed: 0.3, curve: 1 });
     let m = mapComponent?.getMap();
     m.setStyle('mapbox://styles/mapbox/satellite-v9');
 
-		tourPoints.map(({ label, center, zoom, bearing, pitch, rotate, duration }) => {
-			setTimeout(() => {
-				mapComponent?.flyTo({ center: center, zoom: zoom, pitch: pitch, bearing: bearing, speed: 0.3, curve: 1, essential: true });
+    tourPoints.map(({ label, center, zoom, bearing, pitch, rotate, duration }) => {
+      setTimeout(() => {
+        mapComponent?.flyTo({
+          center: center,
+          zoom: zoom,
+          pitch: pitch,
+          bearing: bearing,
+          speed: 0.3,
+          curve: 1,
+          essential: true,
+        });
 
-				setTimeout(() => {
-					rotateCamera(0)
-				currentFocusName = label;
-			}, 3000);
+        setTimeout(() => {
+          rotateCamera(0);
+          currentFocusName = label;
+        }, 3000);
 
-				// .then(() => {
-				// 	if (rotate) {
-				// 		rotate(0)
-				// 	}
-				// });
-				currentFocusName = label;
-			}, duration);
-		});
+        // .then(() => {
+        // 	if (rotate) {
+        // 		rotate(0)
+        // 	}
+        // });
+        currentFocusName = label;
+      }, duration);
+    });
 
-		// m.setFog({
-		// 	'range': [-1, 1.5],
-		// 	'color': 'white',
-		// 	'horizon-blend': 0.1
-		// });
-		// rotateCamera(0);
+    // m.setFog({
+    // 	'range': [-1, 1.5],
+    // 	'color': 'white',
+    // 	'horizon-blend': 0.1
+    // });
+    // rotateCamera(0);
   };
 
   const handleAppearanceMode = (mode) => {
@@ -117,23 +151,21 @@
     } else {
       isDarkMode = !isDarkMode;
     }
-    isDarkMode
-      ? (style = 'mapbox://styles/mapbox/dark-v10')
-      : (style = 'mapbox://styles/mapbox/outdoors-v11');
+    isDarkMode ? (style = 'mapbox://styles/mapbox/dark-v10') : (style = 'mapbox://styles/mapbox/outdoors-v11');
     m.setStyle(style);
   };
 
-	  // update `money` with `productionPerTick` and set a timeout to call itself after `tickSpeed` ms
-
+  // update `money` with `productionPerTick` and set a timeout to call itself after `tickSpeed` ms
 </script>
 
-
 <div class="relative w-screen h-full select-none">
-  <div class="absolute top-0 left-0 z-50 w-screen h-screen bg-white opacity-70">
-    <div class="flex items-center justify-center w-full h-full">
-      <img src="/east-troy-splash.png" alt="East Troy Maps" class="relative -top-40" />
+  {#if showSplash}
+    <div class="absolute top-0 left-0 z-50 w-screen h-screen bg-white opacity-70" transition:fade>
+      <div class="flex items-center justify-center w-full h-full">
+        <img src="/east-troy-splash.png" alt="East Troy Maps" class="relative -top-40" style="animation: fadein 3s;" />
+      </div>
     </div>
-  </div>
+  {/if}
 
   <div class="absolute transition-all duration-300 left-0 z-50 w-full {menuIsOpen ? 'bottom-0' : 'bottom-4'}">
     <div class="w-full max-w-3xl mx-auto text-center">
@@ -150,32 +182,7 @@
           <Logo />
         </button>
 
-        <div class="absolute top-0 left-2 h-[55px] p-2  flex items-center">
-          <button class="flex items-center mr-3 text-xs text-white" title="night light" on:click={handleNightLife}>
-            <div class="w-6 h-6 mr-1 text-white">
-              <FaBeer />
-            </div>
-            {#if menuIsOpen}
-              <span in:fly={{ x: -80, duration: 500 }} out:fly={{ x: -80, duration: 500 }}>Night Life</span>
-            {/if}
-          </button>
-          <button class="flex items-center mr-3 text-xs text-white" title="night light" on:click={handleCruiseNight}>
-            <div class="w-6 h-6 mr-1 text-white">
-              <IoIosCar />
-            </div>
-            {#if menuIsOpen}
-              <span in:fly={{ x: -80, duration: 500 }} out:fly={{ x: -80, duration: 500 }}>Cruise Night</span>
-            {/if}
-          </button>
-					<button class="flex items-center mr-3 text-xs text-white" title="night light" on:click={handleCruiseNight}>
-            <div class="w-6 h-6 mr-1 text-white">
-              <FaMapMarkedAlt />
-            </div>
-            {#if menuIsOpen}
-              <span in:fly={{ x: -80, duration: 500 }} out:fly={{ x: -80, duration: 500 }}>Take a Tour</span>
-            {/if}
-          </button>
-        </div>
+        <div class="absolute top-0 left-2 h-[55px] p-2  flex items-center" />
 
         <div class="absolute top-0 right-2 h-[55px] p-2  flex items-center">
           <button
@@ -199,15 +206,13 @@
             out:fly={{ y: 60, duration: 200 }}
             class="w-full h-full px-3 py-3 pb-32 overflow-x-hidden overflow-y-scroll text-left"
           >
-
-
-					<div class="p-3 m-3 mb-6 text-xs text-white border border-blue-100 rounded">
-						<p>
-							The goal of this experiment is to interact and explore the East Troy area from a geographical
-							perspective. There will be layers of different meta to show/hide as well as community curated
-							contributions. Geoloco information for East Troy, by East Troy.
-						</p>
-					</div>
+            <div class="p-3 m-3 mb-6 text-xs text-white border border-blue-100 rounded">
+              <p>
+                This is an innovation experiment to interact and explore the East Troy area from a geographical
+                perspective. There will be layers of different meta to show/hide as well as community
+                curated content. Geoloco information for East Troy, by East Troy.
+              </p>
+            </div>
 
             <h3 class="mb-2 text-xs text-white uppercase opacity-50">Demos</h3>
             <button on:click={handleGoToEastTroy} class="px-6 py-2 text-sm text-white bg-blue-600 rounded "
@@ -217,6 +222,47 @@
               on:click={() => mapComponent?.flyTo({ center: [ldsbbq[1], ldsbbq[0]], zoom: 16 })}
               class="px-6 py-2 text-sm text-white bg-blue-600 rounded ">LD's BBQ</button
             >
+
+
+<div class="mt-6 space-y-3">
+
+                  <button
+                    class="flex items-center mr-3 text-xs text-white"
+                    title="night light"
+                    on:click={handleNightLife}
+                  >
+                    <div class="w-6 h-6 mr-1 text-white">
+                      <FaBeer />
+                    </div>
+                    <span>Night Life</span>
+                  </button>
+
+                  <button
+                    class="flex items-center mr-3 text-xs text-white"
+                    title="night light"
+                    on:click={handleCruiseNight}
+                  >
+                    <div class="w-6 h-6 mr-1 text-white">
+                      <IoIosCar />
+                    </div>
+
+                    <span>Cruise Night</span>
+                  </button>
+
+                  <button
+                    class="flex items-center mr-3 text-xs text-white"
+                    title="night light"
+                    on:click={handleCruiseNight}
+                  >
+                    <div class="w-6 h-6 mr-1 text-white">
+                      <FaMapMarkedAlt />
+                    </div>
+
+                    <span>Take a Tour</span>
+                  </button>
+</div>
+
+
 
             <h3 class="mt-3 mb-2 text-xs text-white uppercase opacity-50">Restaurants</h3>
 
@@ -231,7 +277,6 @@
               </div>
               <span>Food Truck Fest 2022</span>
             </div> -->
-
           </div>
         {/if}
       </div>
@@ -315,5 +360,14 @@
   html,
   body {
     @apply w-full h-full overflow-hidden;
+  }
+
+  @keyframes fadein {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
   }
 </style>
